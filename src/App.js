1a10f74705b3acc4f6ecd22e6842160b28f15f1e
Qingda Wen <qw9@uw.edu>
2016-11-21 14:19:06 -0800

    Fixes #1

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React, { Component } from 'react';
+import SignUpForm from './TeamSignUp.js';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <div>
+        <div>
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <SignUpForm />
       </div>
     );
   }
